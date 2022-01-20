17c15b1c58b14d6e4fc11b39afbd208c4f123819
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-20 09:24:30 +0100

    Tasks component added

---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks, onToggle, onDelete }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onToggle={onToggle} onDelete={onDelete} />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
